899f665c37cd4dde155fa005647a34b8c7580653
Michael <liukaiyi54@gmail.com>
2017-04-23 21:06:43 +0800

    Initial Commit

---
+++ CallHoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallHoney
+//
+//  Created by Michael on 23/04/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CallHoney/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CallHoney
+//
+//  Created by Michael on 23/04/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
